15551168968f0ece80c4b3b93ae7d775505622fc
Vinicius <viniciusvalentim769@gmail.com>
2025-12-18 12:04:02 -0300

    atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,6 @@ function App() {
     <>
       <Toaster position="top-center" richColors />
       <Home />
-      {/* <div
-          className="relative overflow-hidden rounded-xl bg-linear-to-r from-[#0d0617] to-[#1a0d2e] border border-purple-900/20 shadow-xl group"
-        >
-          teste
-
-        </div> */}
     </>
   )
 }
